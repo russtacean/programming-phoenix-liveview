c4d70206d865fa9f538ec0347f2dcc9ed210c561
Russ Gunther <russell.g@leantaas.com>
2024-05-15 07:53:53 -0400

    Ch 3: Create a seeds script to populate the database with products

--- pento/priv/repo/seeds.exs
+++ pento/priv/repo/seeds.exs
@@ -9,3 +9,29 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Pento.Catalog
+
+products = [
+  %{
+    name: "Chess",
+    description: "The classic strategy game",
+    sku: 5_678_910,
+    unit_price: 10.00
+  },
+  %{
+    name: "Tic-Tac-Toe",
+    description: "The game of X's and O's",
+    sku: 234_653_243,
+    unit_price: 3.00
+  },
+  %{
+    name: "Table Tennis",
+    description: "Bat the ball back and forth. Don't miss!",
+    sku: 53_243_234,
+    unit_price: 12.00
+  }
+]
+
+Enum.each(products, fn product ->
+  Catalog.create_product(product)
+end)
